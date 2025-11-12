b0f42ac1a353ed5ce59dd3e5dc6505e082a5d244
Gundo12 <rumsbraten669@gmail.com>
2025-11-12 10:42:20 +0100

    implemented abstract classes and seperate services for audio processing and gemini api call

--- main.py
+++ main.py
@@ -1,48 +1,10 @@
-import keyboard
-import sounddevice as sd
-import numpy as np
 from faster_whisper import WhisperModel
+from services.audioprocessor.WhisperServiceImpl import WhisperServiceImpl
+from services.chatbot.GeminiServiceImpl import GeminiServiceImpl
 
-import gemini_api
+whisperService = WhisperServiceImpl(WhisperModel("medium", device="cpu", compute_type="int8"),0.25,16000)
+geminiService = GeminiServiceImpl()
 
-SAMPLE_RATE = 16000
-CHUNK_SEC = 0.25
+prompt = whisperService.start_recording()
+print(geminiService.generate_response(prompt))
 
-model = WhisperModel("small", device="cpu", compute_type="int8")
-
-def record_chunk(seconds: float = CHUNK_SEC, samplerate: int = SAMPLE_RATE) -> np.ndarray:
-    audio = sd.rec(int(seconds * samplerate), samplerate=samplerate, channels=1, dtype="float32")
-    sd.wait()
-    return audio.flatten()
-
-def transcribe(audio: np.ndarray, language: str = "de") -> str:
-    segments, info = model.transcribe(audio, language=language, vad_filter=False)
-    return "".join(seg.text for seg in segments).strip()
-
-#--------------------------------------------------------------------------------------------------
-# Aufnahme-Steuerung
-#--------------------------------------------------------------------------------------------------
-print("Halte STRG zum Aufnehmen…")
-keyboard.wait("ctrl")
-
-print("Aufnahme läuft…")
-chunks = []
-while keyboard.is_pressed("ctrl"):
-    chunks.append(record_chunk())
-
-print("Aufnahme beendet.")
-
-if chunks:
-    audio = np.concatenate(chunks).astype(np.float32)
-else:
-    audio = np.array([], dtype=np.float32) 
-
-print(f"Länge aufgenommen: {audio.shape[0]/SAMPLE_RATE:.2f} s")
-
-if audio.size == 0:
-    print("Kein Audio aufgenommen.")
-else:
-    text = transcribe(audio)
-    #print("\nTranskript:\n", text if text else "(leer)")
-
-print(f"Gemini-Anwort: {gemini_api.generate_ai_content(text)}")
---
+++ services/chatbot/GeminiServiceImpl.py
@@ -0,0 +1,23 @@
+from services.chatbot.ChatBotService import ChatbotService
+import os
+import time
+from google import genai
+
+class GeminiServiceImpl(ChatbotService):
+    def __init__(self):
+        self.client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
+
+    def generate_response(self, prompt: str):
+        retries = 2
+        for attempt in range(retries):
+            try:
+                response = self.client.models.generate_content(
+                    model="gemini-2.5-flash",
+                    contents=prompt,
+                )
+                return response.text
+            except Exception as e:
+                if attempt == retries:
+                    raise
+                time.sleep(2)
+                return "Exception gefangen"
