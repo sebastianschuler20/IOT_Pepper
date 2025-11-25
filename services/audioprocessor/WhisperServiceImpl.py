071523cda93fed37f1aac7ace67c684a7dc22607
Gundo12 <rumsbraten669@gmail.com>
2025-11-25 10:11:45 +0100

    added initial prompt

--- services/audioprocessor/WhisperServiceImpl.py
+++ services/audioprocessor/WhisperServiceImpl.py
@@ -10,7 +10,7 @@ class WhisperServiceImpl(AudioProcessorService):
         self.model = model
 
     def transcribe(self, audio: np.ndarray, language: str = "de") -> str:
-        context_prompt = "Gespräch mit dem Roboter Pepper. Pepper wird in den meisten Fällen am Anfang des Satzes gesagt"
+        context_prompt = "Das folgende ist ein Gespräch mit dem humanoiden Roboter namens Pepper. Es wird entweder Smalltalk mit ihm geführt oder ihm wird befohlen zu tanzen oder zu singen. Beispiele für Befehle sind 'Pepper tanz' oder 'Pepper sing'. Beispiele für Smalltalk sind 'Pepper Wie wird das Wetter morgen', 'Pepper Erzähl mir etwas über Angela Merkel', 'Pepper Gib mir Kochrezepte,  Als Anrede wird am Anfang Pepper gesagt"
 
         segments, info = self.model.transcribe(
             audio,
